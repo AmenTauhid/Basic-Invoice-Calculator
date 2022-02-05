894c3d273e0a1ea099e8a48143b6fd6559526978
Amen <aymantauhid2@gmail.com>
2022-02-05 22:26:29 +0530

    Add project files.

---
+++ W2A_InvoiceCalculator/InvoiceCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W2A_InvoiceCalculator
+{
+    class InvoiceCalculator
+    {
+        public double Subtotal;
+        public double TipPercentage;
+
+        public double GetTipAmount()
+        {
+            return Subtotal * TipPercentage / 100;
+        }
+
+        public double GetHst()
+        {
+            return Subtotal * 0.13;
+        }
+
+        public double GetTotal()
+        {
+            return Subtotal + GetTipAmount() + GetHst();
+        }
+    }
+}
+
+
---
+++ W2A_InvoiceCalculator/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace W2A_InvoiceCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //string name = "Ayman";
+            //decimal tax = 13;
+            //Decimal.Parse(subtotal.Text) 
+            //result.Text = $"{Decimal.Parse(subtotal.Text) + Decimal.Parse(tips.Text) + Decimal.Parse(subtotal.Text) * (13 / 100)}";
+        }
+
+        //Ayman 
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            InvoiceCalculator calculator = new InvoiceCalculator();
+
+            try
+            {
+                calculator.Subtotal = double.Parse(subtotal.Text);
+                calculator.TipPercentage = double.Parse(tips.Text);
+                
+                result.Text = $"Tip amount: ${calculator.GetTipAmount()}\n" +
+                              $"Tax Amount: ${calculator.GetHst()}" +
+                              $"\nTotal Amount: ${calculator.GetTotal()}";
+            }
+            catch (Exception)
+            {
+               result.Text = $"Input error";
+            }
+            
+
+        }
+
+        private void Subtotal_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            result.Text = " ";
+        }
+
+        private void Tips_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            result.Text = " ";
+        }
+    }
+}
